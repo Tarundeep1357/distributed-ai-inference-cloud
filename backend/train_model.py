841c47c2739511e8c2db1d8ae2cc98a3171956b8
Tarundeep Singh <tarundeepsinghghotra1@gmail.com>
2026-07-24 00:32:01 +0530

    updated train_model.py

--- backend/train_model.py
+++ backend/train_model.py
@@ -4,10 +4,30 @@ import joblib
 from sklearn.datasets import load_iris
 from sklearn.ensemble import RandomForestClassifier 
 
-def train_model():
+def train_model() -> None:
     iris= load_iris()
 
     x= iris.data
     y= iris.target
 
-    
+    model= RandomForestClassifier(
+        n_estimators= 100,
+        random_state= 42
+    )
+
+    model.fit(x,y)
+
+    model_dir= path("models")
+    models_dir.mkdir(exist_ok= True)
+
+    model_path= models_dir / "iris_model.joblib"
+
+    joblib.dump(model , model_path)
+
+    print(f"model saved to: {model_path}")
+
+
+if __name__== "__main__":
+    train_model()
+
+
